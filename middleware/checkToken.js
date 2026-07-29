4ce6f149d274e1c717c0fb6fae3d6298f42e4ead
soad amr <soadamrfathy@gmail.com>
2026-07-29 18:32:29 +0300

    add middleware check role,token

---
+++ middleware/checkToken.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const User = require("../src/models/user.model");
+
+const checkToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
+
+module.exports = checkToken;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-const { createUser, } = require('../controller/users/createUser.controller');
+const { createUser } = require("../controller/users/createUser.controller");
 
 const { deleteUser } = require("../controller/users/deleteuser.controller");
 
-const {
-  getAllUsers,
-} = require("../controller/users/getAllUsers.controller");
+const { getAllUsers } = require("../controller/users/getAllUsers.controller");
 
-const {
-  getUserById,
-} = require("../controller/users/getUserById.controller");
+const { getUserById } = require("../controller/users/getUserById.controller");
 
 const { updateUser } = require("../controller/users/updateUser.controller");
+const checkAdmin = require("../../middleware/checkRole");
+const checkToken = require("../../middleware/checkToken");
 
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", checkToken, checkAdmin, createUser);
+router.put("/:id", checkToken, checkAdmin, updateUser);
 
-
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-
-router.delete('/:id', deleteUser);
+router.delete("/:id", checkToken, checkAdmin, deleteUser);
 
 module.exports = router;
