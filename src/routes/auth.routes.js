ee5e55bb5a5cc57213e48d5c0feb89ae90aab209
kereto <yoosefcf.0@gmail.com>
2026-07-31 22:46:10 +0300

    user&&doc

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userLogin = require("../controller/auth/auth.controller");
-
-router.post("/", userLogin);
-
-module.exports = router;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userRoutes = require('./user.routes');
 const doctorRoutes = require('./doctor.routes');
-const authRouter = require("./auth.routes");
 
-router.use('/auth', authRouter);
+
+
 
 router.use('/users', userRoutes);
 router.use('/doctors', doctorRoutes);
 module.exports = router;
+ 
+ 
