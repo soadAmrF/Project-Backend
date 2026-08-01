84d5163911c575834e27560bb14dd65f8c8a5704
kereto <yoosefcf.0@gmail.com>
2026-08-01 10:12:16 +0300

    add cnary
    :w:wqq#

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userRoutes = require('./user.routes');
 const doctorRoutes = require('./doctor.routes');
+const authRoutes = require('./auth.routes');
 
+
+router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/doctors', doctorRoutes);
 module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,24 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const { createUser } = require("../controller/users/createUser.controller");
-
 const { deleteUser } = require("../controller/users/deleteuser.controller");
-
 const { getAllUsers } = require("../controller/users/getAllUsers.controller");
-
 const { getUserById } = require("../controller/users/getUserById.controller");
-
 const { updateUser } = require("../controller/users/updateUser.controller");
-
 const checkAdmin = require("../middleware/checkAdmin");
-
 const checkToken = require("../middleware/checkToken");
+const { uploadUserImage } = require("../config/cloudinary");
 
-router.get("/",getAllUsers);
+router.get("/", getAllUsers);
 router.get("/:id", getUserById);
-router.post("/", checkToken, checkAdmin, createUser);
+router.post(
+  "/",
+  checkToken,
+  checkAdmin,
+  uploadUserImage.single("image"),
+  createUser,
+);
 router.put("/:id", checkToken, checkAdmin, updateUser);
-
 router.delete("/:id", checkToken, checkAdmin, deleteUser);
 
 module.exports = router;
