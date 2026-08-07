fb45cf5eeb896ae77ce06a2b333d3c539f9c1e00
soad amr <soadamrfathy@gmail.com>
2026-08-07 20:51:40 +0300

    add getAllMedicalRecords

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -12,6 +12,7 @@ const labTestRoutes = require("./labTest.routes");
 const inventoryRoutes = require("./inventory.routes");
 const inventoryTransactionRoutes = require("./inventoryTransaction.routes");
 const clinicInfoRoutes = require("./clinicInfo.routes");
+const invoiceRoutes = require("./invoice.routes");
 
 router.use("/clinic-info", clinicInfoRoutes);
 router.use("/inventory-transactions", inventoryTransactionRoutes);
@@ -24,25 +25,7 @@ router.use("/users", userRoutes);
 router.use("/doctors", doctorRoutes);
 router.use("/patient", patientRoutes);
 router.use("/appointment", appointmentRoutes);
-
-const userRoutes = require("./user.routes");
-const doctorRoutes = require("./doctor.routes");
-const authRoutes = require("./auth.routes");
-const medicalRecordRoutes = require("./medicalRecord.routes");
-const patientRoutes = require("./patient.route");
-const appointmentRoutes = require("./appointment.routes");
-const labOrderRoutes = require("./labOrder.routes");
-const labTestRoutes = require("./labTest.routes");
-const invoiceRoutes = require("./invoice.routes");
-
-router.use("/labTest", labTestRoutes);
-router.use("/labOrder", labOrderRoutes);
-router.use("/medicalrecord", medicalRecordRoutes);
-router.use("/auth", authRoutes);
-router.use("/users", userRoutes);
-router.use("/doctors", doctorRoutes);
-router.use("/patient", patientRoutes);
-router.use("/appointment", appointmentRoutes);
 router.use("/invoice", invoiceRoutes);
+
 
 module.exports = router;
--- src/routes/medicalRecord.routes.js
+++ src/routes/medicalRecord.routes.js
@@ -6,13 +6,15 @@ const getMedicalRecord = require("../controller/medicalRecord/getMedicalRecord")
 const getPatientMedicalRecords = require("../controller/medicalRecord/getPatientMedicalRecords");
 const updateMedicalRecordNotes = require("../controller/medicalRecord/updateMedicalRecordNotes");
 const deleteMedicalRecord = require("../controller/medicalRecord/deleteMedicalRecord");
+const getAllMedicalRecords = require("../controller/medicalRecord/getAllMedicalRecords");
 
 const checkToken = require("../middleware/checkToken");
 const checkRole = require("../middleware/checkRole");
 
 router.post("/", checkToken, checkRole("admin"), createMedicalRecord);
-router.get("/:id", checkToken, checkRole("admin"), getMedicalRecord);
+router.get("/", checkToken, checkRole("admin"), getAllMedicalRecords);
 router.get("/pama/:id", checkToken,checkRole("admin"), getPatientMedicalRecords);
+router.get("/:id", checkToken, checkRole("admin"), getMedicalRecord);
 router.patch("/:id", checkToken, checkRole("admin"), updateMedicalRecordNotes);
 router.delete("/:id", checkToken, checkRole("admin"), deleteMedicalRecord);
 
